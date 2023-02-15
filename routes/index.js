566c169429a9b5773c85b3001badc08229125cff
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-15 18:03:57 -0500

    Added necessary files

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+// Import our modular routers for /tips and /feedback
+const notesRouter = require('./notes');
+
+const app = express();
+
+app.use('/notess', notesRouter);
+
+module.exports = app;
---
+++ routes/notes.js
@@ -0,0 +1,51 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the tips
+notes.get('/', (req, res) => {
+  readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// DELETE Route for a specific tip
+tips.delete('/:tip_id', (req, res) => {
+  const tipId = req.params.tip_id;
+  readFromFile('./db/tips.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all tips except the one with the ID provided in the URL
+      const result = json.filter((tip) => tip.tip_id !== tipId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/tips.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${tipId} has been deleted 🗑️`);
+    });
+});
+
+// POST Route for a new UX/UI tip
+tips.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      tip_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, '../db/db.json')
+    res.json(`Tip added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+module.exports = tips;
