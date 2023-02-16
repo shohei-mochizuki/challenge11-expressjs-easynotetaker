a7bdee8cfde4d64d7e6bde45eb063f9547119a61
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-15 20:35:52 -0500

    Added comments to servers.js

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
+// Import libraries
 const express = require('express');
 const path = require('path');
-// const { clog } = require('./middleware/clog');
+
+// Import ./api route 
 const api = require('./routes/index.js');
 
+// Port setting for Heroku || Local computer
 const PORT = process.env.PORT || 3001;
 
+// Make express available by call the variable "app"
 const app = express();
-
-// Import custom middleware, "cLog"
-// app.use(clog);
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Make ./api route
 app.use('/api', api);
 
+// Set "public" folder as static file folder
 app.use(express.static('public'));
 
 
-// GET Route for note page
+// GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// Wildcard route to direct users to a 404 page
+// Wildcard route to direct users to a landing page
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
